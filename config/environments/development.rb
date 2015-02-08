a87c7a2e8326579524e89ef28991f9c3b3db236b
aqutw <aquarian.ex@gmail.com>
2015-02-08 13:07:10 +0800

    FINISH implement MovieImage by gem paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
   # Needed for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
  
+  # for paperclip
+  Paperclip.options[:command_path] = "/usr/bin/"
+
 end
